6820da4b951ddf6db5c8f459ac8aa2df6776064e
Joon Seo <jnseojnseo@gmail.com>
2025-08-20 19:57:23 +0900

    feat(organisms): add index file for component exports
    - Introduced an index file to streamline exports for the
      MagicBar, OrganizeModal, SearchResults, and WatchFolderModal
      components, improving module organization and import
      efficiency.

---
+++ src/lib/components/organisms/index.ts
@@ -0,0 +1,4 @@
+export { default as MagicBar } from "./magic-bar.svelte";
+export { default as OrganizeModal } from "./organize-modal.svelte";
+export { default as SearchResults } from "./search-results.svelte";
+export { default as WatchFolderModal } from "./watch-folder-modal.svelte";
