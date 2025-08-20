14f41185c20f5c2562a42d0df0dd65dace4404aa
Joon Seo <jnseojnseo@gmail.com>
2025-08-20 19:56:19 +0900

    feat(molecules): add index file for molecule components
    - Introduced an index file to export molecule components for easier
      imports throughout the application.
    - Components included: SearchInput, FileTreeItem, WatchButton,
      SearchResultItem, WatchedFolderItem, NoResults.

---
+++ src/lib/components/molecules/index.ts
@@ -0,0 +1,6 @@
+export { default as SearchInput } from "./search-input.svelte";
+export { default as FileTreeItem } from "./file-tree-item.svelte";
+export { default as WatchButton } from "./watch-button.svelte";
+export { default as SearchResultItem } from "./search-result-item.svelte";
+export { default as WatchedFolderItem } from "./watched-folder-item.svelte";
+export { default as NoResults } from "./no-results.svelte";
