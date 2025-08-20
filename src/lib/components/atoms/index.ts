2370821a1437b9b78225fce5291b5c5a737ce037
Joon Seo <jnseojnseo@gmail.com>
2025-08-20 19:54:58 +0900

    feat(components): add exports for Icon, Text, Spinner, and DropOverlay
    - Introduced new component exports for better modularity.
    - This allows for easier imports and usage of these components in other parts of the application.

---
+++ src/lib/components/atoms/index.ts
@@ -0,0 +1,4 @@
+export { default as Icon } from "./icon.svelte";
+export { default as Text } from "./text.svelte";
+export { default as Spinner } from "./spinner.svelte";
+export { default as DropOverlay } from "./drop-overlay.svelte";
