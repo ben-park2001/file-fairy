6a8f023a3abd3b3b1c736c8bc78a0dd843cf1c2d
Joon Seo <jnseojnseo@gmail.com>
2025-08-20 19:57:41 +0900

    feat(components): add index file for component exports
    - Introduced an index file to streamline exports for all components.
    - This change enhances the organization of the component architecture
      following the Atomic Design principles.

---
+++ src/lib/components/index.ts
@@ -0,0 +1,13 @@
+// Atomic Design Component Architecture
+
+// Atoms - Basic building blocks
+export * from "./atoms";
+
+// Molecules - Simple combinations of atoms
+export * from "./molecules";
+
+// Organisms - Complex components combining molecules and atoms
+export * from "./organisms";
+
+// Templates - Page-level layouts
+export * from "./templates";
