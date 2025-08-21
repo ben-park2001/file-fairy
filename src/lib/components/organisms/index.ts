cb7bd4c450a33aabe27a811a20e7e62ba84acb1f
Joon Seo <jnseojnseo@gmail.com>
2025-08-21 21:56:47 +0900

    fix(organisms): correct export for OrganizeResults component
    - Updated the export statement for the OrganizeResults component
    - Replaced the incorrect OrganizeModal export with OrganizeResults

--- src/lib/components/organisms/index.ts
+++ src/lib/components/organisms/index.ts
@@ -1,4 +1,4 @@
 export { default as MagicBar } from "./magic-bar.svelte";
-export { default as OrganizeModal } from "./organize-modal.svelte";
+export { default as OrganizeResults } from "./organize-results.svelte";
 export { default as SearchResults } from "./search-results.svelte";
 export { default as WatchFolderModal } from "./watch-folder-modal.svelte";
